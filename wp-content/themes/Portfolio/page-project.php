255f4c727ccc7ded01adee98d96b31b91b6ba2f1
JoyMwende <joymwendekairi@gmail.com>
2023-07-18 12:04:11 +0300

    style changes

--- wp-content/themes/Portfolio/page-project.php
+++ wp-content/themes/Portfolio/page-project.php
@@ -4,7 +4,7 @@
  * Template Name: Project Page
  */
 
- global $wpdb;
+global $wpdb;
 
 $table_name = $wpdb->prefix . 'projects';
 
@@ -21,13 +21,20 @@ get_header();
         <img src="<?php echo $project->project_image; ?>" alt="" style="height: 100%; width: 90%;">
     </div>
     <div class="w-50">
-        <h3><?php echo $project->project_title; ?></h3>
-        <p><?php echo $project->project_desc; ?></p>
+        <h3>
+            <?php echo $project->project_title; ?>
+        </h3>
+        <p>
+            <?php echo $project->project_desc; ?>
+        </p>
         <section>
             <p><b>Technologies Used: </b></p>
-            <p><?php echo $project->technology_used; ?></p>
+            <div class="tech">
+                <p>
+                    <?php echo $project->technology_used; ?>
+                </p>
+            </div>
         </section>
         <a href="<?php echo $project->github_link; ?>" target="_blank">Github Link</a>
-        <?php var_dump($project->github_link); ?>
     </div>
 </div>
